2b1a8911dfbe729ffa243454f22b7aa922fb0c84
HimanshuGorane <himanshug17082001@gmail.com>
2023-02-11 17:29:28 +0530

    update

--- script.js
+++ script.js
@@ -8,20 +8,37 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.filter(student=>student.marks>=50)
+  .map(student=>console.log(`Name:${arr.name},Age:${arr.age}:Marks:${arr.marks}`));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(student => {
+    if (student.marks >= 50) {
+      console.log(
+        `Name: ${student.name}, Age: ${student.age}, Marks: ${student.marks}`
+      );
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = { id: 4, name: "Himanshu", age: "22", marks: 45 };
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let pass = arr.filter(student=>student.marks>=50);
+  console.log(pass);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newStudents = [    { id: 5, name: "Saurabh", age: "21", marks: 65 },    { id: 5, name: "Lakhan", age: "20", marks: 75 },    { id: 6, name: "yakoob", age: "19", marks: 55 }  ];
+  let combinedArray = arr.concat(newStudents);
+  console.log(combinedArray);
 }
